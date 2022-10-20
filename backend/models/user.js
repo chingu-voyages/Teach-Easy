276a30fa5afb4cad0815afa6043f966724fb141c
Mark Linn <marktlinn@gmail.com>
2022-10-20 18:51:15 +0200

    fix: remove username from user schema set email to unique

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 const User = new Schema({
-        username: String,
         firstName: String,
         lastName: String,
+        email: {
+                type: String,
+                unique: true
+        },
         image: String,
         googleId: String,
         secret: String,
