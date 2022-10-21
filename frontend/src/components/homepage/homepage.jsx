735eef9e876d7d0f9091523e8d372748344348ca
trisBouden <tris.bouden@mail.com>
2022-10-21 13:39:13 +0100

    adding features slider

--- frontend/src/components/homepage/homepage.jsx
+++ frontend/src/components/homepage/homepage.jsx
@@ -3,6 +3,7 @@ import section2Image from './../../assets/home-section2.jpg';
 import studentsImage from './../../assets/students.png';
 import section3Image from './../../assets/rocket.png';
 import { Link } from 'react-router-dom';
+import Slider from './slider';
 export default function Homepage() {
   return (
     <>
@@ -56,25 +57,7 @@ export default function Homepage() {
 
         {/* this is gonna be a slider */}
 
-        <div className="slider bg-violet-300/50">
-          <div className="container px-5 m-auto lg:w-[960px] xl:w-[1140px] 2xl:w-[1320px] h-full">
-            <div className="w-full flex flex-wrap gap-[10%] py-16 items-center justify-center md:justify-between">
-              <div className="content w-full md:w-[50%] md:order-2 text-center">
-                <h1 className="text-slate-900 font-black text-5xl sm:text-4xl md:text-left lg:text-5xl tracking-tight text-center dark:text-white">
-                  Learn Anytime Anywhere.
-                </h1>
-                <p className="my-6 text-lg lg:text-xl text-slate-600 text-center md:text-left max-w-3xl mx-auto dark:text-slate-400">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Officia ut consequuntur, optio sed vero vitae beatae nisi!
-                </p>
-              </div>
-              <div className="mt-6 md:mt-0 image md:w-[40%] md:order-1">
-                {/*The images here will be screenshots of the app to show users all the features*/}
-                <img src={section2Image} alt="learn english" />
-              </div>
-            </div>
-          </div>
-        </div>
+        <Slider />
         {/*This section contains the two links to choose user */}
         <div className="section3">
           <div className="container px-5 m-auto lg:w-[960px] xl:w-[1140px] 2xl:w-[1320px] h-full">
