4dc746b7677eb66b44054981ed6d07da3fa7168c
medchakhman <medchakhman@gmail.com>
2022-10-21 15:14:03 +0000

    initial push to merge with homepmage

---
+++ frontend/src/pages/signup.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import SignupForm from "../components/signup"
+
+function Signup() {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  )
+}
+
+export default Signup
