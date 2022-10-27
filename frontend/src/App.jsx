12d4664803ebaee7e3681bcbbfd33e64dd6338b7
medchakhman <medchakhman@gmail.com>
2022-10-27 19:03:55 +0000

    signup pages for student and teacher

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
-import Homepage from "./components/homepage/homepage"
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { useState } from 'react';
+import Homepage from './components/homepage/homepage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignupTeacher from './components/signup/signupTeacher';
+import SignupStudent from './components/signup/signupStudent';
+
 function App() {
   return (
     <div className="app">
       <Router>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/signup" element={<Homepage />} />
+          {/* <Route path="/sign-in" element={<SignIn />} /> */}
+          <Route path="/signup-teacher" element={<SignupTeacher />} />
+          <Route path="/signup-student" element={<SignupStudent />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,9 +1,11 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'students-bg': '#CAC1FF',
+      },
+    },
   },
   plugins: [],
-}
+};
