b7ebed10806c5193f944d56f42912ce17b564bb4
Mark Linn <marktlinn@gmail.com>
2022-10-30 21:47:39 +0100

    Add: rate lesson routes, auth routes remaining to complete

--- backend/controllers/lesson.js
+++ backend/controllers/lesson.js
@@ -10,8 +10,10 @@ const getLessonDoc = async (req,res) => {
     const tags = req.query.tags;
     try {
         const lessons = await lessonDocInDB.find({ tags : { $in : tags.split(',').map(elem=> elem) }});
-        console.log('found', lessons);
-        res.status(200).json(lessons);
+        const arrayLength = lessons[0].rating.rateGiven.length;
+        const averageRating = (lessons[0].rating.rateGiven.reduce((a,b)=> +a + +b))/arrayLength;
+        //average rating is returned for frontend display only
+        res.status(200).json({...lessons, averageRating: averageRating});
     } catch (error) {
         res.status(500).json({error: error.message});
     }
@@ -41,7 +43,6 @@ const postLesson = async(req,res)=> {
         res.status(400).json({error: 'Please make sure all fields are filled', emptyFields});
     }
     try {
-        console.log('title', title, 'image', image)
         const lesson = await lessonDocInDB.create({teacherId: id, title, image, teacherName, lessonDocument, tags, language});
         res.status(200).json(lesson);
     } catch (error) {
@@ -50,7 +51,7 @@ const postLesson = async(req,res)=> {
 } 
 
 const bookLesson = async (req,res) => {
-    const { id, teacherId } = req.params;
+    const { id, teacherId } = req.body;
     try {
         const booking = await teacherDocInDB.findOneAndUpdate({ _id: teacherId});
         booking.nextLessonAttendees.push(id);
@@ -61,4 +62,26 @@ const bookLesson = async (req,res) => {
     }
 };
 
-module.exports = {postLesson, getLessonDoc, bookLesson};
+const rateLesson = async (req,res)=> {
+    //search for lesson by its id
+    const {teacherId, rating, studentId} = req.body;
+    console.log(teacherId, rating, studentId)
+    if(rating > 5 || rating <0) return res.status(400).json({error: "Not a valid rating"});
+    try {
+        const lesson = await lessonDocInDB.findOneAndUpdate({ teacherId: teacherId});
+            if(!lesson.rating.ratedBy.includes(studentId)){
+                lesson.rating.rateGiven.push(rating);
+                lesson.rating.ratedBy.push(studentId);
+                console.log
+                await lesson.save();
+                res.status(200).json(lesson);
+            }
+            else{
+                res.status(400).json({error: "student has already rated this lesson"})
+            }
+    } catch (error) {
+        
+    }
+} 
+
+module.exports = {postLesson, getLessonDoc, bookLesson, rateLesson};
