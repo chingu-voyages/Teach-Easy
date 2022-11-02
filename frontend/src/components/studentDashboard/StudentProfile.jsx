8b99202b1316c53cb180f9ba0c0cad875d0c6537
medchakhman <medchakhman@gmail.com>
2022-11-02 10:48:19 +0000

    student profile: added responsiveness

---
+++ frontend/src/components/studentDashboard/StudentProfile.jsx
@@ -0,0 +1,199 @@
+import React from 'react';
+import ImagePlaceholder from './../../assets/image-placeholder.png';
+import {
+  faStar,
+  faTag,
+  faGraduationCap,
+  faChalkboardUser,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function StudentProfile() {
+  const [tags, setTags] = React.useState(null);
+  const handletags = (e) => {
+    e.preventDefault();
+  };
+  //   React.useEffect(() => {
+  //     console.log(tags.map((e) => console.log(e)));
+  //   });
+  return (
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+      <div className=" py-5 flex flex-col gap-6">
+        <div className="grid-item">
+          <div className="flex-item  flex flex-row gap-3 mb-4">
+            <img
+              src={ImagePlaceholder}
+              className="w-10 h-10 rounded-full"
+              alt="profile picture"
+            />
+            <h3 className="font-bold text-lg">student name</h3>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="font-bold text-xl">Welcome back, student name</p>
+
+            <p className="leading-relaxed">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit a
+              voluptatibus molestiae ullam amet provident dolores delectus aut
+              optio eum
+            </p>
+          </div>
+        </div>
+        <div className="grid-item">
+          <div className="flex-item  flex flex-row gap-3 mb-4">
+            <FontAwesomeIcon
+              icon={faStar}
+              size="2x"
+              className=" text-purple-600"
+            />
+
+            <h3 className="font-bold text-lg">Study Goals</h3>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p>
+              <strong>Starting Level: </strong>A1 - First Steps
+            </p>
+            <p>
+              <strong>Aim: </strong>B2 - upper Intermediate
+            </p>
+            <strong>Reasons why:</strong>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur dolor sit, amet
+              consectetur adipisicing elit. Placeat odio ut aut officiis,{' '}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="py-5 col-span-2 flex flex-col gap-10">
+        <div className="grid-item">
+          <div className="flex-item  flex flex-col gap-3 mb-4">
+            <h2 className="font-bold text-xl">Find Lessons Now</h2>
+            <input
+              type="search"
+              className="justify-self-stretch px-3 py-4 rounded-md mt-2 bg-gray-200"
+              placeholder="Start your learning journey here..."
+            />
+          </div>
+          <div className="flex-item  flex flex-col gap-3 mb-4">
+            <h2 className="font-bold text-xl">
+              <FontAwesomeIcon
+                icon={faTag}
+                size="1x"
+                className=" text-purple-600 mr-2"
+              />
+              Search By Tags
+            </h2>
+            <div className="flex gap-3 mt-3 justify-center flex-wrap">
+              {/* <button className="filter-tag">Beginner</button>
+              <button className="filter-tag">Advanced</button>
+              <button className="filter-tag">Intermediate</button>
+              <button className="filter-tag">Master</button>
+              <button className="filter-tag">Master</button>
+              <button className="filter-tag">Master</button> */}
+              <div className="checkbox-item relative bg-gray-300 px-3 py-2">
+                <label for="beginner" className="z-2">
+                  <input
+                    className="  w-full h-full z-0"
+                    type="checkbox"
+                    name="beginner"
+                    id="beginner"
+                    value="Beginner"
+                  />
+                  Beginner
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid-item">
+          <div className="flex-item  flex flex-col gap-3 mb-4">
+            <h2 className="font-bold text-xl">
+              <FontAwesomeIcon
+                icon={faGraduationCap}
+                size="1x"
+                className=" text-purple-600 mr-2"
+              />
+              Your Next Lesson
+            </h2>
+            <h3 className="mt-3 font-bold">Lesson title:</h3>
+          </div>
+          <div className="flex flex-col gap-3">
+            <p className="leading-relaxed">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit a
+              voluptatibus molestiae ullam amet provident dolores delectus aut
+              optio eum
+            </p>
+            <p>
+              <strong>Date: </strong>10/05/2023
+            </p>
+            <p>
+              <strong>Date: </strong>10/05/2023
+            </p>
+          </div>
+        </div>
+        {/* last item */}
+        <div className="grid-item">
+          <div className="flex-item  flex flex-col gap-3 mb-4">
+            <h2 className="font-bold text-xl">
+              <FontAwesomeIcon
+                icon={faChalkboardUser}
+                size="1x"
+                className=" text-purple-600 mr-2"
+              />
+              Find Your Next Lesson
+            </h2>
+            <h3 className="mt-3 font-bold">Recommended Teachers</h3>
+          </div>
+          <div className="flex flex-col gap-3">
+            <p className="leading-relaxed">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit a
+              voluptatibus molestiae ullam.
+            </p>
+            <div className="flex justify-between gap-8 flex-wrap">
+              <div className="teacher-item p-2">
+                <strong>Alex M.Alberten</strong>
+                <img
+                  src={ImagePlaceholder}
+                  className="w-full mt-2 rounded-md"
+                />
+                <p className="mt-2">
+                  a short description about the lesson and it's goals
+                </p>
+                <p className="mt-2">
+                  <strong>Lesson level: </strong>Basic
+                </p>
+              </div>
+              <div className="teacher-item p-2">
+                <strong>Alex M.Alberten</strong>
+                <img
+                  src={ImagePlaceholder}
+                  className="w-full mt-2 rounded-md"
+                />
+                <p className="mt-2">
+                  a short description about the lesson and it's goals
+                </p>
+                <p className="mt-2">
+                  <strong>Lesson level: </strong>Basic
+                </p>
+              </div>
+              <div className="teacher-item p-2">
+                <strong>Alex M.Alberten</strong>
+                <img
+                  src={ImagePlaceholder}
+                  className="w-full mt-2 rounded-md"
+                />
+                <p className="mt-2">
+                  a short description about the lesson and it's goals
+                </p>
+                <p className="mt-2">
+                  <strong>Lesson level: </strong>Basic
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StudentProfile;
