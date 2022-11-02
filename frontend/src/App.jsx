e6d665f9255c278beff778fefb38463a1233f2b5
medchakhman <medchakhman@gmail.com>
2022-11-02 10:48:19 +0000

    student profile: added responsiveness

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignupTeacher from './components/signup/signupTeacher';
 import SignupStudent from './components/signup/signupStudent';
 import SignIn from './components/signup/SignIn';
+import StudentProfile from './components/studentDashboard/StudentProfile';
+import Dashboard from './components/studentDashboard/Dashboard';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
           <Route path="/login" element={<SignIn />} />
           <Route path="/signup-teacher" element={<SignupTeacher />} />
           <Route path="/signup-student" element={<SignupStudent />} />
+          <Route path="/student-dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     </div>
