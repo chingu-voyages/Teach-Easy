3910abcb54c4d9ffb60593f2b2e7b4f73c0366c7
Mark Linn <marktlinn@gmail.com>
2022-11-06 21:30:38 +0100

    add: techer dash & lesson upload modal components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from './components/signup/SignIn';
 import StudentProfile from './components/studentDashboard/StudentProfile';
 import Dashboard from './components/studentDashboard/Dashboard';
 import TeacherProfile from './components/teacherProfile/teacherProfile';
+import TeacherDashboard from './components/TeacherDashboard/TeacherDash'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/signup-teacher" element={<SignupTeacher />} />
           <Route path="/signup-student" element={<SignupStudent />} />
           <Route path="/student-dashboard" element={<Dashboard />} />
+          <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
           <Route path="/sampleTeacherProfile" element={<TeacherProfile />} />
         </Routes>
       </Router>
