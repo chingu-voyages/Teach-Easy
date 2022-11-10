968f7b611877dbcec9b6799bbe5e8f638cbbbc8a
theborgh <borghiborghi@gmail.com>
2022-11-10 22:51:16 +0100

    fetch profile image

--- frontend/src/components/TeacherDashboard/TeacherDash.jsx
+++ frontend/src/components/TeacherDashboard/TeacherDash.jsx
@@ -3,12 +3,12 @@ import TeacherInfo from './TeacherInfo';
 import { useState } from 'react';
 import UploadModal from './UploadModal';
 
-function TeacherDashboard() {
+function TeacherDashboard({ userId }) {
   const [uploading, setUploading] = useState(false);
   return (
     <div className="main-wrapper bg-gray-200">
       <div className="main-container max-w-6xl mx-auto my-0">
-        <TeacherInfo setUploading={setUploading} />
+        <TeacherInfo setUploading={setUploading} userId={userId} />
       </div>
       {uploading && <UploadModal setUploading={setUploading} />}
     </div>
